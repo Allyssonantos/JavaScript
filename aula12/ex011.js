1e9d4e9aceeb6d316f32123efeae75419206feeb
Allysson Santos <allyssonsantos487@gmail.com>
2022-01-20 22:13:04 -0300

    12

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,12 +1,16 @@
-var idade = 13
+var idade = 19
 if (idade < 16) {
-    console.log('Não vota')
-} else { 
-    if (idade >= 19) {
-        console.log('Voto obrigatorio!')
-    } else {
-        if (idade <= 18) {
-            console.log('Voto opcinal')
+    console.log('Não vota!')
+} else {
+} if (idade < 18 || idade > 65) {
+    console.log(`Voto opcional! Você tem ${idade} anos.`)
+} else {
+    if (idade > 18) {
+        console.log('Voto Obrigatorio')
     }
 }
-}
+
+
+
+
+
