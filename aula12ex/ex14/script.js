2ba51589ee705703cb26591947e36415afe559e6
Allysson Santos <allyssonsantos487@gmail.com>
2022-01-24 22:56:20 -0300

    12 ex 14

--- aula12ex/ex14/script.js
+++ aula12ex/ex14/script.js
@@ -3,17 +3,20 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 8
+    var hora = 19
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //Bom dia!
         img.src = 'manha.jpg'
+        document.body.style.background = '#FF8C00'
     } else if ( hora >= 12 && hora <= 18) {
         //Boa tarde!
         img.src ='tarde.jpg'
+        document.body.style.background = '#CDB7B5'
     } else {
         //Boa noite
         img.src = 'noite.jpg'
+        document.body.style.background = '#607B8B'
     }
     
 }
