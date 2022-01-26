64188fdcb0e807da6816648eddd43b1b48ac75d1
Allysson Santos <allyssonsantos487@gmail.com>
2022-01-25 22:16:18 -0300

    ex 15

--- aula12ex/ex15/script.js
+++ aula12ex/ex15/script.js
@@ -3,12 +3,35 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.lemgth == 0 || Number(fano.value) > ano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(feno.value)
+        var idade = ano - Number(fano.value)
         var gênero = ''
-        if
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+            } else if (idade < 21) {
+                //Jovem
+            } else if (idade < 50) {
+                //Idoso
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+            } else if (idade < 21) {
+                //Jovem
+            } else if (idade < 50) {
+                //Idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
     }
+
 }
