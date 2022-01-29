fcfe4e0ee9472a44ba3082da38aad13461ac8fe5
Allysson Santos <allyssonsantos487@gmail.com>
2022-01-28 22:24:32 -0300

    aula 14 ex 16

---
+++ aula14ex/ex16/script.js
@@ -0,0 +1,9 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fin = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+
+    if (ini.valule.length == 0 || fim.valule.length == 0 || passo.valule.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else
+}
