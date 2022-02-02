621a7a3621c92419c9a3fe06f2718b7025776d5c
Allysson Santos <allyssonsantos487@gmail.com>
2022-02-01 21:51:31 -0300

    Aula 14 ex 17

--- aula14/aula14ex/ex17/script.js
+++ aula14/aula14ex/ex17/script.js
@@ -5,5 +5,12 @@ function tabuada() {
         window.alert('Por favor, digite um numero!')
     } else {
         let n = Number(num.value)
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tap.appendChild(item)
+            c++
+        }
     }
 }
